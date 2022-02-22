e4d7fd9e7cffdeaab19a9b0fae95ab1fc942f466
Cedric Cellier <rixed@happyleptic.org>
2022-02-22 08:40:08 +0100

    Add --sat to smt2-tool to parse and print sat/unset/etc result

--- Smt2Tool.ml
+++ Smt2Tool.ml
@@ -3,12 +3,16 @@
 let main =
   let debug = Array.mem "--debug" Sys.argv in
   let is_model = Array.mem "--model" Sys.argv in
+  let is_sat = Array.mem "--sat" Sys.argv in
   ignore (Parsing.set_trace debug) ;
   let lexbuf = Lexing.from_channel stdin in
   try
     if is_model then
       let resps = Smt2Parser.get_model_response Smt2Lexer.token lexbuf in
       Format.printf "%a@\n" Smt2Types.Response.print resps
+    else if is_sat then
+      let resp = Smt2Parser.check_sat_response Smt2Lexer.token lexbuf in
+      Format.printf "%a@\n" Smt2Types.Response.print resp
     else
       let comms = Smt2Parser.commands Smt2Lexer.token lexbuf in
       Format.printf "%a@\n" Smt2Types.Script.print_commands comms
